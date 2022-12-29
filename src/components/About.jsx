dac60c6352080f77276a80dfbd3fe5df7e28faea
Csandrade1 <andrade.cynthia07@gmail.com>
2022-12-28 18:35:50 -0800

    trying something out

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,6 +12,18 @@ const About = () => {
             About
           </p>
         </div>
+        <div>
+          <iframe
+            // style="border-radius:12px"
+            src="https://open.spotify.com/embed/artist/7HBWZRtvj0QxCFabtLmjrt?utm_source=generator"
+            width="50%"
+            height="352"
+            frameBorder="0"
+            allowfullscreen=""
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+            loading="lazy"
+          ></iframe>
+        </div>
 
         <p className="text-xl mt-20">
           Hello, I'm currently attending Bloomtech Institute.
